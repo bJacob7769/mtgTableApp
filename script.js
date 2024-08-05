03175f901c900fd7278d2b6a39c51f35b05b0214
Jacob <jacob@jacobs-mbp.attlocal.net>
2024-08-04 22:04:59 -0700

    added new files and filled in previous work

---
+++ script.js
@@ -0,0 +1,17 @@
+const names = document.getElementById('names');
+const tableListButt = document.querySelectorAll('#tables');
+const addPlayerButt = document.getElementById('add-player');
+const removePlayerButt = document.getElementById('remove-player');
+const markWinnerButt = document.getElementById('mark-winner');
+const shuffleSeatsButt = codument.getElementById('shuffle-seats');
+
+
+const addPlayer = () => {
+    // names.innerHTML += '<div class="name-cont">Rusty Shackleford</div>';
+    console.log('bdfodsjfdsalkfjdsakjfdklasjf');
+}
+console.log('bdfodsjfdsalkfjdsakjfdklasjf');
+
+addPlayerButt.addEventListener('click', addPlayer);
+
+addPlayer();
